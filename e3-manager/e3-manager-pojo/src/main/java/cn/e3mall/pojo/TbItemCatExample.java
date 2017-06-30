4e9d814bec963e76f9c8cbe228f1084ebb39e805
Administrator <Administrator@CIRLZJ60ARQFUIB>
2017-06-30 19:21:39 +0800

    添加manager工程

--- e3-manager/e3-manager-pojo/src/main/java/cn/e3mall/pojo/TbItemCatExample.java
+++ e3-manager/e3-manager-pojo/src/main/java/cn/e3mall/pojo/TbItemCatExample.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class TbItemCatExample {
+public class TbItemCatExample{
     protected String orderByClause;
 
     protected boolean distinct;
